edf3690296a820c31fcc1ebb92f28bb34b4966fa
Minh Ngo <bbbbbb267@gmail.com>
2023-05-19 01:34:16 +0200

    fix conflict

--- src/main/java/com/ms/tourist_app/domain/entity/Destination.java
+++ src/main/java/com/ms/tourist_app/domain/entity/Destination.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Nationalized;
-import org.mapstruct.control.MappingControl;
 
 import javax.persistence.*;
 import java.util.List;
