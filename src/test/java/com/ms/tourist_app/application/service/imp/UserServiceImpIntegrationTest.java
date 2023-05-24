f1b7a6a595ed1e67dfc7820c59d92e55b1ddd881
Minh Ngo <bbbbbb267@gmail.com>
2023-05-25 01:09:30 +0200

    add TestConfiguration

--- src/test/java/com/ms/tourist_app/application/service/imp/UserServiceImpIntegrationTest.java
+++ src/test/java/com/ms/tourist_app/application/service/imp/UserServiceImpIntegrationTest.java
@@ -1,20 +1,36 @@
 package com.ms.tourist_app.application.service.imp;
 
+import com.ms.tourist_app.application.dai.AddressRepository;
+import com.ms.tourist_app.application.dai.DestinationRepository;
+import com.ms.tourist_app.application.dai.UserRepository;
 import com.ms.tourist_app.application.input.users.UserDataInput;
 import com.ms.tourist_app.application.service.UserService;
+import com.ms.tourist_app.application.utils.JwtUtil;
 import com.ms.tourist_app.config.exception.BadRequestException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceImpIntegrationTest {
-
     @Autowired
     private UserService userService;
+
+    @TestConfiguration
+    static class UserServiceImpTestContextConfiguration {
+        @Bean
+        public UserService userService(UserRepository userRepository, AddressRepository addressRepository,
+                                       DestinationRepository destinationRepository, JwtUtil jwtUtil) {
+            return new UserServiceImp(userRepository, addressRepository, destinationRepository, jwtUtil);
+        }
+    }
 
     @Test
     public void createUser_duplicateEmail() {
